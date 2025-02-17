ed87c2b37204784da075884eda0ebdb02fad8526
isayaksh <isayaksh@gmail.com>
2025-02-17 21:41:50 +0900

    <feat #13> create 방생성 API

--- src/main/java/org/prography/pingpong/domain/room/repository/RoomRepository.java
+++ src/main/java/org/prography/pingpong/domain/room/repository/RoomRepository.java
@@ -3,5 +3,10 @@ package org.prography.pingpong.domain.room.repository;
 import org.prography.pingpong.domain.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+    boolean existsByHostId(Integer hostId);
+
 }
--- src/main/java/org/prography/pingpong/domain/room/service/RoomService.java
+++ src/main/java/org/prography/pingpong/domain/room/service/RoomService.java
@@ -1,7 +1,14 @@
 package org.prography.pingpong.domain.room.service;
 
 import lombok.RequiredArgsConstructor;
+import org.prography.pingpong.domain.room.dto.RoomCreateReqDto;
+import org.prography.pingpong.domain.room.entity.Room;
 import org.prography.pingpong.domain.room.repository.RoomRepository;
+import org.prography.pingpong.domain.room.repository.UserRoomRepository;
+import org.prography.pingpong.domain.user.entity.User;
+import org.prography.pingpong.domain.user.entity.enums.UserStatus;
+import org.prography.pingpong.domain.user.repository.UserRepository;
+import org.prography.pingpong.global.exception.ApiException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,10 +18,43 @@ import org.springframework.transaction.annotation.Transactional;
 public class RoomService {
 
     private final RoomRepository roomRepository;
+    private final UserRoomRepository userRoomRepository;
+    private final UserRepository userRepository;
+
 
     @Transactional
     public void deleteAll() {
         roomRepository.deleteAll();
+    }
+
+    @Transactional
+    public void createRoom(RoomCreateReqDto roomCreateReqDto) {
+
+        /*
+         * 방을 생성하려고 하는 user(userId)의 상태가 활성(ACTIVE)상태일 때만, 방을 생성
+         */
+        Integer userId = roomCreateReqDto.userId();
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(() -> new ApiException("RoomService.createRoom"));
+
+        if(!findUser.getStatus().equals(UserStatus.ACTIVE))
+            throw new ApiException("RoomService.createRoom");
+
+        /*
+         * 방을 생성하려고 하는 user(userId)가 현재 참여한 방이 있다면, 방생성 X
+         */
+        if(roomRepository.existsByHostId(userId))
+            throw new ApiException("RoomService.createRoom");
+
+        if(userRoomRepository.existsByUserId(userId))
+            throw new ApiException("RoomService.createRoom");
+
+        /*
+         * 방은 초기에 대기(WAIT) 상태로 생성
+         */
+        Room room = roomCreateReqDto.create(findUser);
+        roomRepository.save(room);
+
     }
 
 }
